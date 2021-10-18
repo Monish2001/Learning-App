5c057430551cdb0a111ea891f55637af34b2a21e
monish2001 <smonish.2001@gmail.com>
2021-10-18 12:25:49 +0530

    Added completed percentage in SubjectsController

--- Controllers/SubjectsController.cs
+++ Controllers/SubjectsController.cs
@@ -28,7 +28,7 @@ namespace Learning_App.Controllers
             var currentUser = HttpContext.User;
             
             var StudentIdFromJWT = currentUser.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name).Value;
-            int StudentId = Int32.Parse(StudentIdFromJWT);
+            int studentId = Int32.Parse(StudentIdFromJWT);
             
             var SubList = _db.Subjects.Where(s => s.ClassId == class_id).ToList();
             
@@ -41,7 +41,7 @@ namespace Learning_App.Controllers
                     ClassId = SubList[i].ClassId,
                     Name = SubList[i].SubName,
                     Logo = SubList[i].logo,
-                    Percentage = 58
+                    Percentage = CalculateSubjectCompletedPercentage(SubList[i].Id,studentId)
                 };
 
                 ListOfSubjects.Add(responseObj);
@@ -51,6 +51,80 @@ namespace Learning_App.Controllers
             };
             string output = JsonConvert.SerializeObject(subjectResponseObj);
             return Ok(output);
+        }
+
+        public double? CalculateSubjectCompletedPercentage(int subjectId, int studentId)
+        {
+            var chapterList = _db.Chapters.Where(c => c.SubjectId == subjectId).ToList();
+            double? totalCompletionPercentage = 0;
+            double? pdfCompletionPercentage = 0;
+            double? videoCompletionPercentage = 0;
+            int? totalPdfPages = 0;
+            int? totalReadPages = 0;
+            long? totalDuration = 0;
+            long? completedDuration = 0;
+            for(var i=0; i < chapterList.Count; i++)
+            {
+
+                var contentList = _db.Contents.Where(c => c.ChapterId == chapterList[i].Id).ToList();
+                for(var j=0; j<contentList.Count; j++){
+                    if(contentList[j].ContentType == "Pdf"){
+                        IDictionary<string,int?> trackPdfResponse = TrackPDF(contentList[j].Id, studentId);
+                        totalPdfPages += trackPdfResponse["Total"];
+                        totalReadPages += trackPdfResponse["Viewed"];
+                    }
+                    else{
+                        IDictionary<string,long?> trackVideoResponse = TrackVideo(contentList[j].Id, studentId);
+                        totalDuration += trackVideoResponse["Total"];
+                        completedDuration += trackVideoResponse["Completed"];
+                    }
+                }
+            }
+            
+            pdfCompletionPercentage = ((double)totalReadPages/(double)totalPdfPages)*100;
+            videoCompletionPercentage = ((double)completedDuration/(double)totalDuration)*100;
+            totalCompletionPercentage = (pdfCompletionPercentage+videoCompletionPercentage)/2;
+            return totalCompletionPercentage;
+        }
+
+        public IDictionary<string,int?> TrackPDF (int contentId, int studentId)
+        {
+            IDictionary<string, int?> trackPdfPages = new Dictionary<string, int?>();
+            var trackPdfObj = _db.TrackPdf.Where(tp => tp.ContentId == contentId && tp.StudentId == studentId).ToList();
+            int? totalPages = 0;
+            int? viewedPages = 0;
+            if(trackPdfObj.Count()!=0){
+                totalPages = trackPdfObj[0].TotalPages;
+                viewedPages = trackPdfObj[0].ViewedPages;
+                trackPdfPages.Add("Total",totalPages);
+                trackPdfPages.Add("Viewed", viewedPages);
+            }
+            else{
+                trackPdfPages.Add("Total",totalPages);
+                trackPdfPages.Add("Viewed", viewedPages);
+            }
+            return trackPdfPages;
+        }
+
+        public IDictionary<string,long?> TrackVideo (int contentId, int studentId)
+        {
+            IDictionary<string, long?> trackVideos = new Dictionary<string, long?>();
+            var trackVideoObj = _db.TrackVideos.Where(tv => tv.ContentId == contentId && tv.StudentId == studentId).ToList();
+            long? totalDurationInMins = 0;
+            long? completedDurationInMins = 0;
+            
+            if(trackVideoObj.Count()!=0){
+                totalDurationInMins = trackVideoObj[0].Totalduration;
+                completedDurationInMins = trackVideoObj[0].Completeduration;
+                trackVideos.Add("Total",totalDurationInMins);
+                trackVideos.Add("Completed", completedDurationInMins);
+            }
+            else{
+                trackVideos.Add("Total",totalDurationInMins);
+                trackVideos.Add("Completed", completedDurationInMins);
+            }
+            
+            return trackVideos;
         }
     }
 }
--- Models/Serializer/SubjectsSerializer.cs
+++ Models/Serializer/SubjectsSerializer.cs
@@ -5,7 +5,7 @@ public class Subject
     public int Id { get; set; }
     public string Name { get; set; }
     public int ClassId{get; set;}
-    public int? Percentage { get; set; }
+    public double? Percentage { get; set; }
     public string Logo { get; set; }
 }
 
